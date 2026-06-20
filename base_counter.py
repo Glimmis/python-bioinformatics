f592c7428b23615671cfa60500cf024e39222132
Glimmis <129577636+Glimmis@users.noreply.github.com>
2026-06-20 18:27:02 +0200

    Base_counter.py
    Added DNA base counter

--- base_counter.py
+++ base_counter.py
@@ -1,1 +1,11 @@
+def base_counter(dna):
+    print("DNA-sekvens:", dna)
+    print("Totalt antal baser =", len(dna))
+    print()
 
+    for bas in "ATCG":
+        print(bas, "=", dna.count(bas))
+
+dna = "ATCGGCTAGCGCGATAT"
+
+base_counter(dna)
